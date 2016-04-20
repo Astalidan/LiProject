37db2e147dfecf94503f64e12bf1e19a7ace56be
darkonark <darkonark@yahoo.com>
2016-04-20 13:51:13 -0400

    Update D_FF.vhd
    added comment

--- Code/D_FF.vhd
+++ Code/D_FF.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity D_FF is
+entity D_FF is --if you look hard enough you'll find that its actually a T flipflop as opposed to a D flipflop
    port(
 		clk,d : in std_logic;
 		q : out std_logic);
